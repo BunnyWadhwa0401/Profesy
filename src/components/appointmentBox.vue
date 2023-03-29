57cc6b12798ba0924ce39232947958a39f85adda
Yuvraj Goyal <68333785+mrManiac-uv@users.noreply.github.com>
2023-03-29 13:21:44 +0530

    Mini Project Done

--- src/components/appointmentBox.vue
+++ src/components/appointmentBox.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
-const props = defineProps({ date: String, day: Number });
+const props = defineProps({ date: String, day: Number, number: Number });
 </script>
 
 <template>
-  <section class="grid rounded-[16px_16px_24px_24px] h-[9.25rem] w-[10rem] bg-white shadow-xl shadow-grey/10">
+  <section class="grid rounded-[16px_16px_24px_24px] h-[8rem] w-[10rem] bg-white shadow-xl shadow-grey/10">
     <div class="mt-6">
-      <h5 class="grid justify-center font-heading text-dark font-medium text-lg">{{ date }}</h5>
-      <h5 class="grid justify-center font-text text-grey pt-1">{{ day }}</h5>
+      <h5 class="grid justify-center font-heading text-dark font-medium text-lg">{{ props.date }}</h5>
+      <h5 class="grid justify-center font-text text-grey pt-1">{{ props.day }}</h5>
     </div>
     <div class="flex rounded-[0px_0px_16px_16px] bg-black text-white justify-center">
-      <h5 class="grid items-center font-heading">2</h5>
+      <h5 class="grid items-center font-heading">{{ props.number }}</h5>
     </div>
   </section>
 </template>
--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,27 +1,54 @@
 <script setup>
 import Header from "../components/Header.vue";
 import OrderStages from "../components/OrderStages.vue";
+import { onMounted, ref } from "vue";
+import gsap from "gsap";
+
+onMounted(() => {
+  const timeLine = gsap.timeline({
+    Defaults: { Easing: "Expo.easeInOut" },
+  });
+  timeLine
+    .fromTo(".fadeIn",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 1,
+      }
+    )
+    .fromTo(
+      ".tile div",
+      {
+        y: -50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.2,
+      },
+      "-=0.5"
+    )
+})
 </script>
 
 <template>
   <main class="Product-Page">
     <Header title="Albatross Crop Jumpsuit" />
     <div class="grid grid-cols-[2fr_3fr] h-[75vh] mt-10">
-      <div class="ml-16">
-        <img
-          class="fixed rounded-3xl h-[33rem]"
-          src="../images/3109.png"
-          alt="Albatros Crop Jumpsuit"
-        />
+      <div class="fadeIn ml-16">
+        <img class="fixed rounded-3xl h-[33rem]" src="../images/3109.png" alt="Albatros Crop Jumpsuit" />
       </div>
       <div>
         <section>
           <h5 class="font-text text-sm font-bold">FOR</h5>
-          <h5 class="font-heading text-4xl mt-1 font-normal">Sanjana Singhania</h5>
+          <h5 class="font-heading text-4xl mt-1 font-normal">Preeti Verma</h5>
           <h5 class="font-text text-sm mt-5 font-bold">STATUS</h5>
           <h5 class="font-heading text-4xl mt-1 font-normal">In Progress</h5>
         </section>
-        <section class="grid gap-4 mt-5">
+        <section class="tile grid gap-4 mt-5">
           <OrderStages stage="Khaaka" :svgNum="0" />
           <OrderStages stage="Dyeing" :svgNum="1" />
           <OrderStages stage="Handwork" :svgNum="2" />
